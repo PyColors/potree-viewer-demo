ec92494ea1705655bd120c57f6b1ead7c3ec522b
Patrice <patweb44@gmail.com>
2020-02-03 15:30:06 +0100

    Setting up project

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const port = 8022;
+
+// Serve static files from libs and pointclouds directory
+app.use("/libs", express.static("./libs/"));
+app.use("/pointclouds", express.static("./pointclouds/"));
+
+// Render main.html, specify the root
+app.get("/", (req, res) => {
+  res.sendFile("main.html", { root: "." });
+});
+
+app.listen(port, () => {
+  console.log("Server listening on port " + port);
+});
