a9a2baf3b41bc16b58f3fd4b56c1db7b921d1a8f
Patrice <patweb44@gmail.com>
2020-02-10 17:42:10 +0100

    Adding styles and data

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const port = 8022;
 // Serve static files from libs and pointclouds directory
 app.use("/libs", express.static("./libs/"));
 app.use("/pointclouds", express.static("./pointclouds/"));
+app.use("/style", express.static("./style"));
 
 // Render main.html, specify the root
 app.get("/", (req, res) => {
